8521426503a83049308d4edcd5783f88472afc32
Kayden Clark <kaydenclark725@gmail.com>
2021-01-26 15:29:09 -0700

    Fixed  history

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import {
     useHistory,
-    useLocation
 } from "react-router-dom";
 import { AuthContext } from '../context/auth-context'
 import '../styles/Login.css'
 
 export const Login = () => {
     let history = useHistory();
-    let location = useLocation();
-
-    let { from } = location.state || { from: { pathname: "/" } };
+    // console.log(history)
+    
     let login = (event, contextFunc) => {
         event.preventDefault()
         contextFunc(() => {
-            history.replace(from);
+            history.goBack();
         });
     };
     
