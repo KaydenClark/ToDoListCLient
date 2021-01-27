fc1b1ba4208fab13180ab1051da243278afae8ea
Kayden Clark <kaydenclark725@gmail.com>
2021-01-27 01:58:19 -0700

    fixed isues from last push

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import {
     useHistory,
-    useLocation
 } from "react-router-dom";
 import { AuthContext } from '../context/auth-context'
 import '../styles/Login.css'
 
 export const Login = () => {
     let history = useHistory();
-    let location = useLocation();
-
-    let { from } = location.state || { from: { pathname: "/" } };
+    
     let login = (event, contextFunc) => {
         event.preventDefault()
         contextFunc(() => {
-            history.replace(from);
+            history.goBack();
         });
     };
     
